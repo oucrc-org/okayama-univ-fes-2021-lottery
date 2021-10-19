599dbc3cca8e84b63839f4dce1d9c0b456ad0508
chishige1217200 <daichicchi01@gmail.com>
2021-10-19 10:19:37 +0900

    Add: lottery.py 雛形

---
+++ lottery.py
@@ -0,0 +1,23 @@
+# 環境変数の読み込み
+import config
+# 乱数用ライブラリ
+import random
+# MySQL用ライブラリ
+import pymysql.cursors
+
+# DBに接続
+conn = pymysql.connect(
+    host=config.host,
+    user=config.user,
+    passwd=config.passwd,
+    db=config.db,
+    charset='utf8',
+    cursorclass=pymysql.cursors.Cursor)
+
+# 受信処理記述（DB問い合わせ）
+# サーバに登録されている固有キーと名前をリストに順に格納
+# 例 a = [[2417,岡大太郎],[13954, ことちゃん], ...]という形になるように
+
+conn.close()  # DBから切断
+
+# randomライブラリを使って，配列からいくつかの要素を弾き出せるようにする
