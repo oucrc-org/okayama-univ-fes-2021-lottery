28d8a34a6f4c3b0d2cab27a2a075bc66d1dd3151
chishige1217200 <daichicchi01@gmail.com>
2021-10-19 10:19:49 +0900

    Add: config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# .env ファイルをロードして環境変数へ反映
+import os
+from dotenv import load_dotenv
+
+# 環境変数を読み込み
+load_dotenv()
+
+# 環境変数を参照
+host = os.getenv('HOST')
+user = os.getenv('USER')
+passwd = os.getenv('PASSWD')
+db = os.getenv('DB')
