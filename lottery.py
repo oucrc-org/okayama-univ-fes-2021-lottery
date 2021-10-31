8b5578746b94d9682dd22aaff0afb197b37dbec1
chishige1217200 <daichicchi01@gmail.com>
2021-10-31 13:29:47 +0900

    Clean: コメント整理 コード無効化

--- lottery.py
+++ lottery.py
@@ -20,18 +20,18 @@ def auto_lot(present_id, conn):  # 抽選するプレゼントID, 接続情報
         cursor.execute(sql, (present_id))
         result = cursor.fetchall()
     # サーバに登録されているuser_idをリストに順に格納
-    # 例 user_id = [1,2,12]
-    # TODO:カラムstamps（要受信）の個数分同じuser_idを入れると良い（事実上の抽選確率となる）
+    # 例 user_id = [1,1,1,1,2,2,2,4,4,12,13,13]
+    # カラムstampsの個数分同じuser_idを入れると良い（事実上の抽選確率となる）
     user_id = []
     for i in range(len(result)):
         for j in range(0, result[i][1]):
             user_id.append(result[i][0])
 
-    print(user_id)
+    # print(user_id)
 
     remained_stock = manual_lot(present_stock, user_id)
 
-# print("Remained stock = " + remained_stock)  # 残数表示
+    print("Remained stock = " + str(remained_stock))  # 残数表示
     print("")
     return
 # end of auto_lot
@@ -45,12 +45,12 @@ def manual_lot(stock, appliciants):  # int, int[]
         a = random.randrange(len(appliciants))
         winner.append(appliciants[a])
         b = appliciants[a]
-    # TODO:randomライブラリを使って，配列からいくつかの要素を弾き出して新しいリストに格納する，ただし，一度出た人はリストから除外
+    # randomライブラリを使って，配列からいくつかの要素を弾き出して新しいリストに格納する，ただし，一度出た人はリストから除外
     # 一度出た人を除外するというのは，その人のuser_idをappliciants上からすべて削除することを言う．
     # appliciantsの長さが0になったら強制的にbreak
         appliciants = [i for i in appliciants if i != b]
     print(winner)
-    return  stock - len(winner)
+    return stock - len(winner)
 # end of auto_lot
 
 
@@ -71,7 +71,7 @@ with conn.cursor() as cursor:  # 抽選するプレゼントの種類数を自�
     result = cursor.fetchone()
 present_count = result[0]
 
-for i in range(1, present_count + 1):  # 抽選するプレゼントの種類分ループ
-    auto_lot(i, conn)
+for i in range(present_count):  # 抽選するプレゼントの種類分ループ
+    auto_lot(i + 1, conn)
 
 conn.close()  # DBから切断
